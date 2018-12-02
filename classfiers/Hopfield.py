14475fe8372c95c94a6f6f7773c7ea1877c20113
FluenceYHL <37389680+FluenceYHL@users.noreply.github.com>
2018-12-02 13:08:53 +0800

    Add files via upload

--- classfiers/Hopfield.py
+++ classfiers/Hopfield.py
@@ -4,14 +4,13 @@ import numpy
 import random
 
 
-def make_noise(origin):
+def make_noise(origin, probablity=0.25):
     noise = origin
     n, m = noise.shape
     for i in range(n):
         for j in range(m):
             if(random.randint(0, 24) % 6 == 0):
                 noise[i][j] = 1 - noise[i][j]
-    paint(noise)
     return noise
 
 
@@ -34,35 +33,60 @@ def sigmoid(net):
     return 1 if(net > 0) else -1
 
 
+class Hopfield():
+    def __init__(self, origin):
+        self.m, self.n = origin.shape
+        self.feature_len = self.m * self.n
+        self.weights = numpy.zeros(
+            (self.feature_len, self.feature_len), dtype=numpy.uint8)
+
+    def memorize(self, origin):
+        origin.resize(self.feature_len)
+        for i in range(self.feature_len):
+            for j in range(i):
+                self.weights[i][j] += origin[i] * origin[j]
+                self.weights[j][i] = self.weights[i][j]
+
+    def repair(self, noise):
+        noise.resize(self.feature_len, 1)
+        cnt = 0
+        while(True):
+            changed = False
+            paint(noise.reshape(self.m, self.n), 'noise' + str(cnt) + '.png')
+            cv2.waitKey()
+            cnt += 1
+            for i in range(self.feature_len):
+                ans = sigmoid(self.weights[i].dot(noise))
+                if(ans != noise[i]):
+                    changed = True
+                noise[i] = ans
+            if(changed == False):
+                break
+        noise.resize(self.m, self.n)
+        paint(noise, 'repaired.png')
+        cv2.destroyAllWindows()
+
+
 if __name__ == '__main__':
     origin = numpy.load('./npys/hopfield/origin/4.npy')
-
-    m, n = origin.shape
-    feature_len = m * n
-    origin.resize(feature_len)
-    weights = numpy.zeros((feature_len, feature_len), dtype=numpy.uint8)
-    for i in range(feature_len):
-        for j in range(i):
-            weights[i][j] += origin[i] * origin[j]
-            weights[j][i] = weights[i][j]
-    weights.resize(feature_len, feature_len)
+    one = Hopfield(origin)
+    one.memorize(origin)
 
     noise = numpy.load('./npys/hopfield/noise/4.npy')
-    noise.resize(feature_len, 1)
-    cnt = 0
-    while(True):
-        changed = False
-        paint(noise.reshape(m, n), 'noise' + str(cnt) + '.png')
-        cnt += 1
-        for i in range(feature_len):
-            ans = sigmoid(weights[i].dot(noise))
-            if(ans != noise[i]):
-                changed = True
-            noise[i] = ans
-        if(changed == False):
-            break
-    noise.resize(m, n)
-    paint(noise, 'repaired.png')
+    one.repair(noise)
+    for i in range(10):
+        noise = numpy.load('./npys/hopfield/noise/4_' + str(i) + '.npy')
+        one.repair(noise)
 
     cv2.waitKey()
     cv2.destroyAllWindows()
+
+    # origin = numpy.load('./npys/hopfield/origin/4.npy')
+    # for i in range(10):
+    #     noise = make_noise(origin)
+    #     paint(noise, 'yhl.png')
+    #     cv2.waitKey()
+    #     numpy.save('./npys/hopfield/noise/4_' + str(i) + '.npy', noise)
+
+    # cv2.waitKey()
+    # cv2.destroyAllWindows()
