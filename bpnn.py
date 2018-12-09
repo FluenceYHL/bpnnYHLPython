0264517d09c3bad65eb60d41385e3119156037d9
FluenceYHL <37389680+FluenceYHL@users.noreply.github.com>
2018-12-09 13:22:56 +0800

    Update bpnn.py

--- bpnn.py
+++ bpnn.py
@@ -4,9 +4,11 @@ import math
 import time
 import numpy
 import datetime
+import warnings
 import matplotlib.pyplot as plt
 # self
 import classfiers.samples
+import images.fileFilter
 
 
 def sigmoid(x):
@@ -39,12 +41,10 @@ class BPNN():
 
     def backward(self, inpt, target):
         out_daoshu = (target - self.out_out) * dSigmoid(self.out_out)
-        self.hid_out.resize(1, self.hideSize)
-        inpt.resize(1, self.inputSize)
-
-        self.wout += self.lrt * self.hid_out.T * out_daoshu
+        self.wout += self.lrt * \
+            self.hid_out.reshape(self.hideSize, 1) * out_daoshu
         hid_daoshu = dSigmoid(self.hid_out) * numpy.dot(self.wout, out_daoshu)
-        self.whid += self.lrt * inpt.T * hid_daoshu
+        self.whid += self.lrt * inpt.reshape(self.inputSize, 1) * hid_daoshu
 
     def recognize(self, inpt):
         return numpy.argmax(self.forward(inpt))
@@ -52,7 +52,7 @@ class BPNN():
 
 class Mnist():
     def __init__(self):
-        self.bpnn = BPNN(784, 100, 10, 0.4)
+        self.bpnn = BPNN(784, 100, 10, 0.13)
 
     def train_and_test(self):
         train_features, train_labels, test_features, test_labels = classfiers.samples.Samples(
@@ -88,6 +88,49 @@ class Mnist():
         numpy.save('./npys/yhl_wout.npy', self.bpnn.wout)
 
 
+class Self():
+    def __init__(self, inputSize, hideSize, outputSize, lrt):
+        self.inputSize = inputSize
+        self.hideSize = hideSize
+        self.outputSize = outputSize
+        self.lrt = lrt
+        self.bpnn = BPNN(inputSize, hideSize, outputSize, lrt)
+
+    def train_and_test(self, train_file, train_answers, test_file, test_answers, iters=1):
+        train_labels = numpy.load(train_answers)
+        test_labels = numpy.load(test_answers)
+        file_list = images.fileFilter.filter(train_file)
+        test_list = images.fileFilter.filter(test_file)
+
+        for it in range(iters):
+            count = 0
+            for path in file_list:
+                img = cv2.imread(path, 0)
+                img.resize(self.inputSize)
+                target = numpy.zeros(self.outputSize)
+                target[int(train_labels[count])] = 1
+                count = count + 1
+                self.bpnn.forward(img)
+                self.bpnn.backward(img, target)
+            count = 0
+            correct = 0
+            for path in test_list:
+                img = cv2.imread(path, 0)
+                img.resize(self.inputSize)
+                judge = self.bpnn.recognize(img)
+                if(judge == test_labels[count]):
+                    correct = correct + 1
+                count = count + 1
+            accuracy = correct / len(test_labels)
+            print("准确率  :  " + str(accuracy))
+
+        numpy.save("./npys/whid.npy", self.bpnn.whid)
+        numpy.save("./npys/wout.npy", self.bpnn.wout)
+
+
 if __name__ == '__main__':
-    one = Mnist()
-    one.train_and_test()
+    warnings.filterwarnings('ignore')
+
+    two = Self(784, 100, 10, 0.01)
+    two.train_and_test('./images/handled', './npys/answers.npy',
+                       './images/handled', './npys/answers.npy', 40)
